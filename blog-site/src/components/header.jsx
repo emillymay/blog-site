c910b986517043b6f73ea7d108d8e0de94f03329
emily <emillymay@LAPTOP-FPN7UJU6>
2024-06-03 13:57:00 -0600

    header setup

--- blog-site/src/components/header.jsx
+++ blog-site/src/components/header.jsx
@@ -1,8 +1,20 @@
-
+import banner from '../assets/banner.png';
+import { CiYoutube } from "react-icons/ci";
 
 function Header() {
     return (
-        <div>Header</div>
+        <div className='flex justify-between items-center'>
+            <img src={banner} className='w-[180px]' />
+            <ul className='flex gap-4 md:gap-14'> 
+                <li className='hover:font-bold cursor-pointer'>Home</li>
+                <li className='hover:font-bold cursor-pointer'>About Us</li>
+                <li className='hover:font-bold cursor-pointer'>Contact Us</li>
+            </ul>
+            <button className='bg-red-400 rounded-full text-white flex items-center p-2'>
+  Subscribe <CiYoutube className='ml-3 text-[20px]'/>
+</button>
+
+        </div>
 
 
     )
